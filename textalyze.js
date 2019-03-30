3f17cb1584f530b894421dfde1ec038e5a85511b
matheushf <hffmatheus@gmail.com>
2019-03-30 09:43:43 -0300

    tests v0.2

--- textalyze.js
+++ textalyze.js
@@ -30,17 +30,14 @@ result = itemCounts(array);
 console.log(result);
 
 /**
- * Takes an arbitrary String as input and returns an Array of all the characters in the string
- * @param {String}
- * @returns {Array}
+ * Takes an arbitrary String as input
+ * and returns an Array with the counting of all the characters in the string
+ * @param {String} arbitrary string
+ * @returns {Array} a Map with the counting of each char
  */
-function stringCharsToArray(string = '') {
+function stringCharsToArray(string) {
   const arrayString = string.split('');
   return itemCounts(arrayString);
 }
-const string = 'testing it';
-result = stringCharsToArray(string);
-console.log(result);
 
-
-module.exports = { itemCounts };
+module.exports = { itemCounts, stringCharsToArray };
